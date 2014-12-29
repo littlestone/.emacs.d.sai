4297a4478041ccd07a94c1f2149cd1dd25097ea1
littlestone <razorsniper@gmail.com>
2014-12-29 14:05:52 -0500

    Set default coding systems to UTF-8

--- settings/sane-defaults.el
+++ settings/sane-defaults.el
@@ -39,7 +39,6 @@
   (set-terminal-coding-system 'utf-8)
   (set-keyboard-coding-system 'utf-8)
   (setq-default buffer-file-coding-system 'utf-8)
-
   ;; Treat clipboard input as UTF-8 string first; compound text next, etc.
   (setq x-select-request-type '(UTF8_STRING COMPOUND_TEXT TEXT STRING)))
 
