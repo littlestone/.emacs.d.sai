5a7f6734c82d4d959c1e83b9707ae55f993ab217
littlestone <razorsniper@gmail.com>
2014-12-29 14:03:23 -0500

    Set default coding systems to utf-8

--- settings/sane-defaults.el
+++ settings/sane-defaults.el
@@ -32,6 +32,17 @@
         desktop-restore-forces-onscreen nil)
   (desktop-save-mode 1))
 
+;; Set default coding systems to UTF-8 
+(progn
+  (prefer-coding-system 'utf-8)
+  (set-default-coding-systems 'utf-8)
+  (set-terminal-coding-system 'utf-8)
+  (set-keyboard-coding-system 'utf-8)
+  (setq-default buffer-file-coding-system 'utf-8)
+
+  ;; Treat clipboard input as UTF-8 string first; compound text next, etc.
+  (setq x-select-request-type '(UTF8_STRING COMPOUND_TEXT TEXT STRING)))
+
 ;; Write backup files to own directory
 (setq backup-directory-alist
       `(("." . ,(expand-file-name
