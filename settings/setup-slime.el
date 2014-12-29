f6df64a087d11cfc905481349714460e15ad9b6f
littlestone <razorsniper@gmail.com>
2014-12-29 10:17:55 -0500

    Enable syntax highlighting for common lisp in SLIME's REPL

--- settings/setup-slime.el
+++ settings/setup-slime.el
@@ -5,12 +5,32 @@
 (require 'slime)
 
 ;; Default common lisp complier
-(setq inferior-lisp-program "sbcl")
-(add-hook 'lisp-mode-hook (lambda () (slime-mode t)))
-(add-hook 'inferior-lisp-mode-hook (lambda () (inferior-slime-mode t)))
+(progn
+  (setq inferior-lisp-program "sbcl")
+  (add-hook 'lisp-mode-hook (lambda () (slime-mode t)))
+  (add-hook 'inferior-lisp-mode-hook (lambda () (inferior-slime-mode t)))
+  (require 'slime-autoloads) ; 注意这里加载的是 slime-autoloads，而不是 slime，要不然C-c C-c等很多功能都没有
+  (slime-setup '(slime-fancy)))
 
-(require 'slime-autoloads) ; 注意这里加载的是 slime-autoloads，而不是 slime，要不然C-c C-c等很多功能都没有
+;; Get syntax highlighting for common lisp in SLIME's REPL
+(progn
+  (defvar slime-repl-font-lock-keywords lisp-font-lock-keywords-2)
+  (defun slime-repl-font-lock-setup ()
+    (setq font-lock-defaults
+          '(slime-repl-font-lock-keywords
+            ;; From lisp-mode.el
+            nil nil (("+-*/.<>=!?$%_&~^:@" . "w")) nil
+            (font-lock-syntactic-face-function
+             . lisp-font-lock-syntactic-face-function))))
 
-(slime-setup '(slime-fancy))
+  (add-hook 'slime-repl-mode-hook 'slime-repl-font-lock-setup)
+  (defadvice slime-repl-insert-prompt (after font-lock-face activate)
+    (let ((inhibit-read-only t))
+      (add-text-properties
+       slime-repl-prompt-start-mark (point)
+       '(font-lock-face
+         slime-repl-prompt-face
+         rear-nonsticky
+         (slime-repl-prompt read-only font-lock-face intangible))))))
 
 (provide 'setup-slime)
